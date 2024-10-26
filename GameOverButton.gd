76bd651b328636cdaf6c0c1b2d6a8936b950616a
psysh14 <psysh14@nottingham.ac.uk>
2024-10-26 21:32:55 +0100

    fixed game over screen scene

--- GameOverButton.gd
+++ GameOverButton.gd
@@ -3,4 +3,4 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _pressed() -> void:
-	get_tree().change_scene_to_file("res://Lvls/test_lvl.tscn")
+	get_tree().change_scene_to_file("res://Lvls/test_scene.tscn")
