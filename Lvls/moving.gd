0e659b6ff60f1507c95d395d86dd190eb5136162
psysh14 <psysh14@nottingham.ac.uk>
2024-10-26 16:32:10 +0100

    finished the thing finally

---
+++ Lvls/moving.gd
@@ -0,0 +1,11 @@
+extends CharacterBody3D
+
+var velocityTing = 2.0
+	
+func _physics_process(delta: float) -> void:
+	const bounds = 4.5
+	if self.position.z > bounds or  self.position.z < -bounds:
+		velocityTing *= -1
+	self.position.z += velocityTing * delta
+	
+	move_and_slide()
