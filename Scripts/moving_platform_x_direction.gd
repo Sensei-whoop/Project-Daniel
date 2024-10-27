b7429f6e192976ffbd50e7c1372bff5057069907
psysh14 <psysh14@nottingham.ac.uk>
2024-10-27 09:42:27 +0000

    improved moving platform level

--- Scripts/moving_platform_x_direction.gd
+++ Scripts/moving_platform_x_direction.gd
@@ -6,6 +6,7 @@ var timer = 0.0
 var platformSpeed = 2.0
 
 func _physics_process(delta: float) -> void:
+	velocity.y = 0
 	timer += delta
 	self.position.x += delta * velocityTing
 	if timer > maxTime:
