09b639635ee9055141f7d68fb57a2a9ed8e8d5da
Bonmon2 <62666119+Bonmon2@users.noreply.github.com>
2024-10-27 10:24:37 +0000

    finished first level

---
+++ Lvls/next_level_1.gd
@@ -0,0 +1,6 @@
+extends Area3D
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+		get_parent_node_3d().get_tree().change_scene_to_file("res://Lvls/MovingPlatform.tscn")
